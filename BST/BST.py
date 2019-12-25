1333984e9df8493d4834749dca2dc51846a04f60
CTMurray <murracam@mail.gvsu.edu>
2019-12-25 18:55:25 -0500

    completed insertion, inorder, preorder, postorder.

--- BST/BST.py
+++ BST/BST.py
@@ -1,18 +1,15 @@
-from BSTNode import BSTNode
-
 # The basic operations supported are by binary trees are
 # searching, traversal, insertion, and deletion.
+# printing inorder, preorder, and postorder.
+
+from BSTNode import BSTNode
 
 class BST:
     #create an empty tree with a root node
     def __init__(self):
         self.root = BSTNode(None)
+        #node count
         self.count = 0
-        # self.head = None
-        # self.parent = None
-        # self.data = None
-        # self.left = None
-        # self.right = None
 
     def insert(self, data):
 
@@ -23,49 +20,74 @@ class BST:
 
         # pointer to root of tree
         temp = self.root
-        while temp is not None:
 
-            #node to insert
-            new_node = BSTNode(data)
+        while temp is not None:
 
             # traverse left sub tree
             if data < temp.data:
-                #empty left child
+                # if empty left child
                 if temp.left == None:
-                    temp.left = new_node
+                    temp.left = BSTNode(data)
 
                     prev = temp
                     temp = temp.left
                     self.count += 1
                     return
+                else:
+                    temp = temp.left
 
-            if data > temp.data:
+            elif data > temp.data:
                 # empty right child
                 if temp.right == None:
-                    temp.right = new_node
+                    temp.right = BSTNode(data)
                     prev = temp
                     temp = temp.right
                     self.count += 1
                     return
+                else:
+                    temp = temp.right
+
+            else:
+                print("Duplicate entry")
+                break
 
 
+    #check if empty
+    #Traverse left
+    #Visit node - print data
+    #traverse right
+    #left – root – right
+    def inorder(self, node):
+        if node is None:
+            return
 
+        self.inorder(node.left)
+        print(node.data)
+        self.inorder(node.right)
 
-                    #temp.left.parent = temp
-                    #self.count += 1
-                    #self.root = self.root.left
+    #check if empty
+    # Visit node - print data
+    # Traverse left
+    # traverse right
+    # root – Left – right
+    def preorder(self, node):
+        if node is None:
+            return
+        print(node.data)
+        self.preorder(node.left)
+        self.preorder(node.right)
 
-            #temp = temp.left
-    def printTree(self):
-        temp = self.root
-        if self.root == None:
-            print("Empty tree!")
-
-        #printing left subtree
-        while temp is not None:
-            print(temp.data)
-            temp = temp.left
-
+    # check if empty
+    # Traverse left
+    # traverse right
+    # Visit node - print data
+    # Left – right - root
+    def postorder(self, node):
+        if node is None:
+            return
+        self.postorder(node.left)
+        self.postorder(node.right)
+        print(node.data)
 
 
 if __name__ == "__main__":
@@ -73,6 +95,28 @@ if __name__ == "__main__":
 
         BST.insert(10)
         BST.insert(5)
+        BST.insert(15)
+        BST.insert(1)
+        BST.insert(9)
+        BST.insert(1) #test duplicate
+        BST.insert(14)
+        BST.insert(20)
+
+        #BST Layout
+                    #10
+            #5              #15
+        #1      #9      #14     #20
+
+
         print("Node count is ", BST.count)
-        BST.printTree()
+        print("=============Preorder===================")
+        #expected output 10, 5, 1, 9, 15, 14, 20
+        BST.preorder(BST.root)
+        print("=============Inorder===================")
+        #expected output 1, 5, 9, 10, 14, 15, 20
+        BST.inorder(BST.root)
+        print("=============Postorder===================")
+        #expected output 1, 9, 5, 14, 20, 15, 10
+        BST.postorder(BST.root)
+
 
